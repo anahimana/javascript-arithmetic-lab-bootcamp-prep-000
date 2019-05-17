daed5c8a259bc552d9cfc9a89e3209ee97bbcccf
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-17 22:49:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,48 +4,31 @@ function add(a, b) {
   return a + b;
 }
 
-// console.log(add(1, 2));
-
 function subtract(a, b) {
   return a - b;
 }
-
-// console.log(substract(4, 2));
 
 function multiply(a, b) {
   return a * b;
 }
 
-// console.log(multiply(1, 2));
-
 function divide(a, b) {
   return a / b;
 }
-
-// console.log(divide(1, 2));
 
 function inc(n) {
   return n += 1;
 }
 
-// console.log(inc(1));
-
 function dec(n) {
   return n -= 1;
 }
-
-// console.log(inc(1));
 
 function makeInt(n) {
   return parseInt(n, 10);
 }
 
-// console.log(makeInt("5", 10));
 
 function preserveDecimal(n) {
   return parseFloat(n);
 }
-
-console.log(preserveDecimal(2.024));
-console.log(preserveDecimal("Cook"));
-
